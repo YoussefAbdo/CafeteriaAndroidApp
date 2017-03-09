cb30390629cd3ce3444f0e874d8247fd6503a278
YoussefAbdo <mryoussefjoe@gmail.com>
2017-03-09 14:47:44 +0200

    activity_basket added XML file edited
    activity_menu_item_details.xml edited

---
+++ app/src/main/java/com/example/asucomputerengineeringteam/finalcafeteriaandroidmobileapp2017/Activities/Basket.java
@@ -0,0 +1,15 @@
+package com.example.asucomputerengineeringteam.finalcafeteriaandroidmobileapp2017.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asucomputerengineeringteam.finalcafeteriaandroidmobileapp2017.R;
+
+public class Basket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+    }
+}
