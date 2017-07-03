24ba0a39ee7e736d8dc21530b068854334ad6413
YoussefAbdo <mryoussefjoe@gmail.com>
2017-07-03 02:43:40 +0200

    Contact_us activity modified

--- app/src/main/java/com/example/asucomputerengineeringteam/finalcafeteriaandroidmobileapp2017/Activities/ContactUsActivity.java
+++ app/src/main/java/com/example/asucomputerengineeringteam/finalcafeteriaandroidmobileapp2017/Activities/ContactUsActivity.java
@@ -9,53 +9,39 @@ import android.widget.Button;
 
 import com.example.asucomputerengineeringteam.finalcafeteriaandroidmobileapp2017.R;
 
+import static com.example.asucomputerengineeringteam.finalcafeteriaandroidmobileapp2017.R.id.send_email_4;
+
 public class ContactUsActivity extends AppCompatActivity {
 
-    Button send_email_1,send_email_2,send_email_3,send_email_4;
+    Button send_email_2,send_email_4;
     //yahoo,gmail,hotmail,facebook
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
 
-
-
-        send_email_1 = (Button)findViewById(R.id.send_email_1);
         send_email_2 = (Button)findViewById(R.id.send_email_2);
-        send_email_3 = (Button)findViewById(R.id.send_email_3);
         send_email_4 = (Button)findViewById(R.id.send_email_4);
 
-
-        send_email_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("https://maktoob.yahoo.com/?p=us")));
-            }
-        });
         send_email_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW ,Uri.parse("https://accounts.google.com/signin")));
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, "mryoussefjoe@gmail.com");
+                intent.putExtra(Intent.EXTRA_CC, "mryoussefjoe@gmail.com");
+                intent.putExtra(Intent.EXTRA_TEXT, "Dear developer,");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
             }
         });
-
-        send_email_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW ,Uri.parse("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1498398671&rver=6.7.6643.0&wp=MBI_SSL_SHARED&wreply=https:%2F%2Fmail.live.com%2Fdefault.aspx&lc=1033&id=64855&mkt=en-us&cbcxt=mai")));
-
-            }
-        });
-
         send_email_4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW ,Uri.parse("https://www.facebook.com/")));
-
+                startActivity(new Intent(Intent.ACTION_VIEW ,Uri.parse("https://www.facebook.com/groups/181138878994734/")));
             }
         });
-
-
-
     }
 }
